749e9fb5457fab0d3032210b669810c6b9af6242
felipf <felipereiraforte@gmail.com>
2023-08-31 16:14:05 -0700

    tasks done

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""type-annotated function sum_mixed_list 
+which takes a list mxd_lst of integers 
+and floats and returns their sum as a float."""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum of list of ints and floats"""
+    return sum(mxd_lst)
---
+++ 9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Annotate the below functions parameters 
+and return values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst] """
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return list of tuples of sequence and int"""
+    return [(i, len(i)) for i in lst]
