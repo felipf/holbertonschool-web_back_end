ab2b3aa7eccb0662ce8d1cd725f8ac5bbf1865d1
felipf <felipereiraforte@gmail.com>
2023-09-03 18:01:43 -0700

    changed files omg

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Write an asynchronous coroutine that takes
+in an integer argument (max_delay, with a default value of 10)
+named wait_random that waits for a random delay
+between 0 and max_delay (included and float value)
+seconds and eventually returns it.
+
+Use the random module."""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Returns:
+        Generator[float, None, None]: [description]
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
+async def main():
+    """
+    Returns:
+        [type]: [description]
+    """
+    async for i in async_generator():
+        print(i)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Import wait_random from the previous
+python file that you have written
+and write an async routine called wait_n that
+takes in 2 int arguments (in this order): n
+and max_delay. You will spawn wait_random n
+times with the specified max_delay.
+
+wait_n should return the list of all the delays
+(float values). The list of the delays should be
+in ascending order without using sort() because of concurrency."""
+import asyncio
+import random
+from typing import List
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay seconds"""
+    i = random.uniform(0, max_delay)
+    await asyncio.sleep(i)
+    return i
+
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """spawn wait_random n times with the specified max_delay"""
+    tasks = []
+    delays = []
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+    for task in asyncio.as_completed(tasks):
+        delays.append(await task)
+    return delays
+
+
+if __name__ == "__main__":
+    asyncio.run(wait_n())
