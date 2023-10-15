cfd958423738f96dc84d22a5a3566141ac3a429a
felipf <felipereiraforte@gmail.com>
2023-10-14 19:42:32 -0700

    fixed task and 9 done

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,17 @@
 export default function cleanSet(set, startString) {
-  return new Set(
-    [...set].filter(
-      (item) => typeof item === 'string' && item.startsWith(startString)
-    )
-  );
+  const cleanValues = [];
+
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+  for (const value of set) {
+    if (
+      value !== undefined &&
+      typeof startString === 'string' &&
+      value.startsWith(startString)
+    ) {
+      cleanValues.push(value.substrin(startString.length));
+    }
+  }
+  return cleanValues.join('-');
 }
