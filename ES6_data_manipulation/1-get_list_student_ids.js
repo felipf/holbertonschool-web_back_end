17b2a6423505e6dff99f2c711d75e790af826916
felipf <felipereiraforte@gmail.com>
2023-10-14 19:09:06 -0700

    fixed and correctly done task3 I guess

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,8 @@
 function getListStudentIds(students) {
-  if (Array.isArray(students)) {
-    return students.map((student) => student.id);
+  if (!Array.isArray(students)) {
+    return [];
   }
+  return students.map((student) => student.id);
 }
 
 export default getListStudentIds;
