db9814a2c5eb8bdd919b82101148c925747935d0
felipf <felipereiraforte@gmail.com>
2023-10-14 18:31:50 -0700

    final user promise

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
+    const signUp = {
+      status: results[0].status,
+      value: results[0].value,
+    };
+    const upload = {
+      status: results[1].status,
+      value: results[1].reason.toString(),
+    };
+    return [signUp, upload];
+  });
+}
