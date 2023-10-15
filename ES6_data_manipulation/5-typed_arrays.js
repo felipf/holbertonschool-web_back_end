2bcfe408954cf6dd977cf48b5f8474e64e29da06
felipf <felipereiraforte@gmail.com>
2023-10-14 19:24:54 -0700

    two tasks

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (postion < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+
+  data.setInt8(position, value);
+  return data;
+}
